714071167dd2daf86f0ecb4e85d379b0ea500a3f
Md Aktar Ahmed <amdaktar86@gmail.com>
2025-12-17 17:40:31 +0600

    task submit btn disable

--- src/pages/AllContest/AllContest.jsx
+++ src/pages/AllContest/AllContest.jsx
@@ -1,5 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
-import React from "react";
+import React, { useState } from "react";
 import useAxios from "../../hooks/useAxios";
 import ContestCard from "./ContestCard";
 import useAuth from "../../hooks/useAuth";
@@ -8,6 +8,7 @@ import Loder from "../../components/Loder/Loder";
 const AllContest = () => {
   const { darkMode } = useAuth();
   const axiosSecure = useAxios();
+  const [activeTab, setActiveTab] = useState("All");
   const { isLoading, data: contests = [] } = useQuery({
     queryKey: ["approved-contest", "Confirmed"],
     queryFn: async () => {
@@ -15,14 +16,45 @@ const AllContest = () => {
       return res.data;
     },
   });
+  const tabs = [
+    "All",
+    "Logo Design",
+    "Photography",
+    "Business Idea",
+    "Landing Page UI",
+  ];
+  const filteredContests =
+    activeTab === "All"
+      ? contests
+      : contests.filter((contest) => contest.contestCategory === activeTab);
 
-  if (isLoading) return <Loder />;
   return (
     <div
       className={`w-full max-w-[1440px] mx-auto  ${
         darkMode ? "bg-black text-white" : "bg-white"
       }`}
     >
+      {/* tabs  */}
+      <div className="tabs tabs-boxed mb-6">
+        {tabs.map((tab) => (
+          <button
+            key={tab}
+            className={`tab font-semibold ${
+              activeTab === tab
+                ? "text-primary"
+                : darkMode
+                ? "text-gray-400"
+                : ""
+            }`}
+            onClick={() => setActiveTab(tab)}
+          >
+            {tab}
+          </button>
+        ))}
+      </div>
+
+      {/* contests */}
+
       <div className="md:max-w-8/12 mx-auto text-center py-10 ">
         <h1 className="text-4xl font-bold text-center ">
           All <span className="text-primary">Contest</span>
@@ -35,17 +67,21 @@ const AllContest = () => {
         </p>
       </div>
       <p className="pl-3">
-        Total Contest:{" "}
+        Total Contest:
         <span className="text-secondary ">
-          {contests.length < 10 && "0" + contests.length}
+          {filteredContests.length < 10 && "0" + filteredContests.length}
         </span>{" "}
       </p>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-        {contests.map((contest) => (
-          <ContestCard key={contest._id} contest={contest}></ContestCard>
-        ))}
-      </div>
+      {isLoading ? (
+        <Loder />
+      ) : (
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+          {filteredContests.map((contest) => (
+            <ContestCard key={contest._id} contest={contest}></ContestCard>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
--- src/pages/AllContest/ContestCardDetails.jsx
+++ src/pages/AllContest/ContestCardDetails.jsx
@@ -1,6 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import React, { useRef } from "react";
-import { Link, useParams } from "react-router";
+import { data, Link, useParams } from "react-router";
 import useAxios from "../../hooks/useAxios";
 import Loder from "../../components/Loder/Loder";
 import ContestNotFoundPage from "../../components/ContestNotFoundPage/ContestNotFoundPage";
@@ -20,6 +20,7 @@ const ContestCardDetails = () => {
     register,
     handleSubmit,
     formState: { errors },
+    reset,
   } = useForm();
 
   //contest card details data fetch here
@@ -46,7 +47,20 @@ const ContestCardDetails = () => {
   const hasParticipated = participation?.participated ?? false;
   console.log(participation);
 
-  if (isLoading) return <Loder />;
+  //submit-task for status check and btn disable
+  const { data: submissionData, refetch } = useQuery({
+    queryKey: ["submit-task", id, user?.uid],
+    queryFn: async () => {
+      const res = await axiosSecure.get(
+        `/submit-task?contestId=${id}&userId=${user?.uid}`
+      );
+      return res.data; // Eta { isSubmitted: true/false } return korbe
+    },
+    enabled: !!id && !!user?.uid,
+  });
+
+  const hasSubmitted = submissionData?.isSubmitted || false;
+  console.log(hasSubmitted);
   if (!contest) return <ContestNotFoundPage />;
 
   const {
@@ -62,8 +76,8 @@ const ContestCardDetails = () => {
     status,
     participantsCount,
   } = contest;
-  // console.log(user);
 
+  //payment button
   const hanleRegisterAndPayment = async (contest) => {
     // console.log(contest);
 
@@ -105,7 +119,9 @@ const ContestCardDetails = () => {
     axiosSecure.post("/submit-task", submitInfo).then((res) => {
       console.log(res.data);
       if (res.data.insertedId) {
+        reset();
         submitModalRef.current.close();
+        refetch();
         setTimeout(() => {
           Swal.fire({
             position: "center",
@@ -119,6 +135,7 @@ const ContestCardDetails = () => {
       } else {
         Swal.fire({
           position: "center",
+          icon: "error",
           title: res.data.message || "Submission failed",
           showConfirmButton: true,
         });
@@ -126,190 +143,221 @@ const ContestCardDetails = () => {
     });
   };
 
+  const onSubmit = (data) => {
+    handleModalSubmit(contest, data);
+  };
+
+  const isContestEnd = new Date(contestDeadline) < new Date();
+
   return (
-    <div
-      className={`w-full max-w-[1440px] mx-auto p-2 lg:p-2  ${
-        darkMode ? "text-white bg-gray-800" : "bg-white"
-      }`}
-    >
-      <div className="flex items-center justify-center mt-10 ">
-        <FaArrowLeft />
-        <Link to="/all-contests" className="ml-2">
-          {" "}
-          Go Back
-        </Link>
-      </div>
-      <div className="text-center lg:max-w-8/12 mx-auto my-10">
-        <h1 className="text-3xl font-bold ">
-          Contest <span className="text-secondary">Details</span>
-        </h1>
-        <p>
-          Join this exciting contest to showcase your skills, compete with
-          talented participants, gain recognition, and win amazing prizes. Don’t
-          miss this opportunity to prove yourself and stand out.
-        </p>
-      </div>
-
-      {/* card content wrap  */}
-      <div>
-        {/* img and title and description  */}
-        <div>
-          <div className="w-full lg:h-[500px]  grid lg:grid-cols-3 gap-4">
-            <div className="lg:relative h-full w-full lg:col-span-2 overflow-hidden">
-              <img
-                className="w-full object-cover"
-                src={contestImage}
-                alt={contestImage}
-              />
-              {/* gradient overlay */}
-              <div className="lg:absolute lg:inset-0 lg:bg-linear-to-t from-gray-900/80 via-gray-900/40 to-transparent"></div>
-              {/* title and description  */}
-              <div className="lg:absolute  lg:bottom-10 lg:left-5 mt-5 lg:mt-0 ">
-                <h1 className="lg:text-white text-4xl font-bold">
-                  {contestTitle}
-                </h1>
-                <p className="lg:text-white max-w-4/5 mt-2">
-                  {contestDescription}
-                </p>
-              </div>
-            </div>
-            <div className="h-full hidden  lg:flex flex-col gap-4 overflow-hidden">
-              <div className="flex-1 w-full overflow-hidden">
-                <img
-                  className="w-full object-cover"
-                  src={contestImage}
-                  alt={contestImage}
-                />
-              </div>
-              <div className="flex-1  overflow-hidden">
-                <img
-                  className="w-full object-cover"
-                  src={contestImage}
-                  alt={contestImage}
-                />
-              </div>
-            </div>
+    <>
+      {isLoading ? (
+        <Loder />
+      ) : (
+        <div
+          className={`w-full max-w-[1440px] mx-auto p-2 lg:p-2  ${
+            darkMode ? "text-white bg-black" : "bg-white"
+          }`}
+        >
+          <div className="flex items-center justify-center mt-10 ">
+            <FaArrowLeft />
+            <Link to="/all-contests" className="ml-2">
+              {" "}
+              Go Back
+            </Link>
           </div>
-        </div>
-        {/* content  */}
-        <div className="mt-10 grid grid-cols-1 md:grid-cols-3 gap-4 p-5 bg-secondary/10">
-          {/* left */}
-          <div className="md:col-span-2 ">
-            <div>
-              <p className="lg:max-w-2/3">
-                <span className="font-semibold text-xl">
-                  Full Task and Instruction:
-                </span>{" "}
-                <br /> {contestTaskInstruction}
-              </p>
-            </div>
-
-            <div className="my-10">
-              <p className=" mt-5 ">
-                Category: <br />
-                <span className=" text-xl font-semibold">
-                  {contestCategory}
-                </span>
-              </p>
-              <p className="mt-5">
-                Prize Money: <br />
-                <span className="text-3xl font-bold">
-                  {" "}
-                  ${contestPrizeMoney}
-                </span>
-              </p>
-
-              <p className="mt-5">Participants: {participantsCount}</p>
-            </div>
-
-            <div>
-              <p>Contest Created By- </p>
-              <h3 className="text-2xl font-bold">{creatorName}</h3>
-              {/* <p>{creatorEmail}</p> */}
-            </div>
+          <div className="text-center lg:max-w-8/12 mx-auto my-10">
+            <h1 className="text-3xl font-bold ">
+              Contest <span className="text-secondary">Details</span>
+            </h1>
+            <p>
+              Join this exciting contest to showcase your skills, compete with
+              talented participants, gain recognition, and win amazing prizes.
+              Don’t miss this opportunity to prove yourself and stand out.
+            </p>
           </div>
-          {/* right */}
-          <div className=" flex flex-col justify-end items-end w-full ">
-            <div className="w-full p-5 shadow-sm  bg-white rounded-lg ">
-              <div>
-                <Countdown contestDeadline={contestDeadline} />
-              </div>
-              <div className="flex gap-5 my-5">
-                <p>Status:</p>
-                <p>
-                  {status === "Confirmed" ? (
-                    <span className="text-green-500">Active</span>
-                  ) : (
-                    <span>Completed</span>
-                  )}
-                </p>
-              </div>
 
-              <div className="my-5">
-                <p>Contest Winner:</p>
-                <p className="text-xl font-bold">
-                  Winner name will appear here
-                </p>
-              </div>
-              <div className="space-y-5">
-                <button
-                  onClick={() => hanleRegisterAndPayment(contest)}
-                  disabled={hasParticipated}
-                  className={`bg-linear-to-r from-primary to-secondary w-full py-2 text-xl text-white cursor-pointer`}
-                >
-                  Register & Pay ${contestEntryFee}
-                </button>
-                <button
-                  disabled={!hasParticipated}
-                  className="btn w-full text-xl "
-                  onClick={() => handleSubmitModal()}
-                >
-                  Submit Task
-                </button>
-              </div>
-
-              {/* modal for submit task */}
-              <dialog
-                ref={submitModalRef}
-                className="modal modal-bottom sm:modal-middle"
-              >
-                <div className="modal-box">
-                  <form method="dialog" className="absolute right-3 top-3 ">
-                    <button className="btn btn-sm btn-circle bg-linear-to-r from-primary to-secondary text-white">
-                      ✕
-                    </button>
-                  </form>
-                  <div className="pt-10">
-                    <h3 className="font-semibold text-xl text-primary mb-2">
-                      Submit Important Links and Text
-                    </h3>
-                    <hr className="border border-primary" />
-
-                    <form
-                      onSubmit={handleSubmit((data) =>
-                        handleModalSubmit(contest, data)
-                      )}
-                    >
-                      <textarea
-                        rows={5}
-                        {...register("taskText")}
-                        className=" border-2  border-gray-400 rounded-lg p-2 outline-0  focus:border-2 focus:border-primary w-full my-5"
-                        placeholder="Write contest details..."
-                      ></textarea>
-                      <input
-                        type="submit"
-                        value="Submit Now"
-                        className="btn w-full bg-linear-to-r from-primary to-secondary text-white"
-                      />
-                    </form>
+          {/* card content wrap  */}
+          <div>
+            {/* img and title and description  */}
+            <div>
+              <div className="w-full lg:h-[500px]  grid lg:grid-cols-3 gap-4">
+                <div className="lg:relative h-full w-full lg:col-span-2 overflow-hidden">
+                  <img
+                    className="w-full object-cover"
+                    src={contestImage}
+                    alt={contestImage}
+                  />
+                  {/* gradient overlay */}
+                  <div className="lg:absolute lg:inset-0 lg:bg-linear-to-t from-gray-900/80 via-gray-900/40 to-transparent"></div>
+                  {/* title and description  */}
+                  <div className="lg:absolute  lg:bottom-10 lg:left-5 mt-5 lg:mt-0 ">
+                    <h1 className="lg:text-white text-4xl font-bold">
+                      {contestTitle}
+                    </h1>
+                    <p className="lg:text-white max-w-4/5 mt-2">
+                      {contestDescription}
+                    </p>
                   </div>
                 </div>
-              </dialog>
+                <div className="h-full hidden  lg:flex flex-col gap-4 overflow-hidden">
+                  <div className="flex-1 w-full overflow-hidden">
+                    <img
+                      className="w-full object-cover"
+                      src={contestImage}
+                      alt={contestImage}
+                    />
+                  </div>
+                  <div className="flex-1  overflow-hidden">
+                    <img
+                      className="w-full object-cover"
+                      src={contestImage}
+                      alt={contestImage}
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+            {/* content  */}
+            <div
+              className={`mt-10 grid grid-cols-1 md:grid-cols-3 gap-4 p-5 ${
+                darkMode ? "bg-black text-white" : " bg-secondary/10"
+              }`}
+            >
+              {/* left */}
+              <div className="md:col-span-2 ">
+                <div>
+                  <p className="lg:max-w-2/3">
+                    <span className="font-semibold text-xl">
+                      Full Task and Instruction:
+                    </span>{" "}
+                    <br /> {contestTaskInstruction}
+                  </p>
+                </div>
+
+                <div className="my-10">
+                  <p className=" mt-5 ">
+                    Category: <br />
+                    <span className=" text-xl font-semibold">
+                      {contestCategory}
+                    </span>
+                  </p>
+                  <p className="mt-5">
+                    Prize Money: <br />
+                    <span className="text-3xl font-bold">
+                      {" "}
+                      ${contestPrizeMoney}
+                    </span>
+                  </p>
+
+                  <p className="mt-5">Participants: {participantsCount}</p>
+                </div>
+
+                <div>
+                  <p>Contest Created By- </p>
+                  <h3 className="text-2xl font-bold">{creatorName}</h3>
+                  {/* <p>{creatorEmail}</p> */}
+                </div>
+              </div>
+              {/* right */}
+              <div className=" flex flex-col justify-end items-end w-full ">
+                <div
+                  className={`${
+                    darkMode ? "bg-gray-900 text-white" : "bg-white"
+                  } w-full  p-5 shadow-sm  rounded-lg `}
+                >
+                  <div>
+                    <Countdown contestDeadline={contestDeadline} />
+                  </div>
+                  <div className="flex gap-5 my-5">
+                    <p>Status:</p>
+                    <p>
+                      {status === "Confirmed" ? (
+                        <span className="text-green-500">Active</span>
+                      ) : (
+                        <span>Completed</span>
+                      )}
+                    </p>
+                  </div>
+
+                  <div className="my-5">
+                    <p>Contest Winner:</p>
+                    <p className="text-xl font-bold">
+                      Winner name will appear here
+                    </p>
+                  </div>
+                  <div className="space-y-5">
+                    <button
+                      onClick={() => hanleRegisterAndPayment(contest)}
+                      disabled={hasParticipated || isContestEnd}
+                      className={`bg-linear-to-r from-primary to-secondary w-full py-2 text-xl text-white cursor-pointer ${
+                        hasParticipated || isContestEnd
+                          ? "opacity-50 cursor-not-allowed"
+                          : "cursor-pointer"
+                      } `}
+                    >
+                      {hasParticipated || isContestEnd
+                        ? "Already Registered"
+                        : `Register & Pay ${contestEntryFee}`}
+                    </button>
+                    <button
+                      disabled={!hasParticipated || hasSubmitted}
+                      className={` py-2 w-full text-xl cursor-pointer ${
+                        !hasParticipated || hasSubmitted
+                          ? "btn-disabled opacity-50 cursor-not-allowed"
+                          : "cursor-pointer"
+                      }`}
+                      onClick={() => handleSubmitModal()}
+                    >
+                      {hasSubmitted ? "Task Already Submitted" : "Submit Task"}
+                    </button>
+                  </div>
+
+                  {/* modal for submit task */}
+                  <dialog
+                    ref={submitModalRef}
+                    className="modal modal-bottom sm:modal-middle"
+                  >
+                    <div className="modal-box">
+                      <form method="dialog" className="absolute right-3 top-3 ">
+                        <button className="btn btn-sm btn-circle bg-linear-to-r from-primary to-secondary text-white">
+                          ✕
+                        </button>
+                      </form>
+                      <div className="pt-10">
+                        <h3 className="font-semibold text-xl text-primary mb-2">
+                          Submit Important Links and Text
+                        </h3>
+                        <hr className="border border-primary" />
+
+                        <form onSubmit={handleSubmit(onSubmit)}>
+                          <textarea
+                            rows={5}
+                            {...register("taskText", { required: true })}
+                            className=" border-2  border-gray-400 rounded-lg p-2 outline-0  focus:border-2 focus:border-primary w-full mt-5"
+                            placeholder="Write contest details..."
+                          ></textarea>
+                          {errors.taskText?.type === "required" && (
+                            <p className="text-red-500">
+                              Task Text is required
+                            </p>
+                          )}
+                          <input
+                            type="submit"
+                            value="Submit Now"
+                            className="btn w-full bg-linear-to-r from-primary to-secondary text-white mt-5"
+                          />
+                        </form>
+                      </div>
+                    </div>
+                  </dialog>
+                </div>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
